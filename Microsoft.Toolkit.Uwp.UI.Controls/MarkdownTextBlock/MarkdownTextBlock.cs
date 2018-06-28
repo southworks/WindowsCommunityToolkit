8234c7bb4ae969682418be72637d38c5b02a7478
Barbara Martinez <barbara.martinez@southworks.com>
2018-06-28 15:52:07 -0300

    add memory manage feature

--- Microsoft.Toolkit.Uwp.UI.Controls/MarkdownTextBlock/MarkdownTextBlock.cs
+++ Microsoft.Toolkit.Uwp.UI.Controls/MarkdownTextBlock/MarkdownTextBlock.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.Toolkit.Uwp.UI.Controls.Markdown.Render;
+using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -16,30 +17,72 @@ namespace Microsoft.Toolkit.Uwp.UI.Controls
         /// <summary>
         /// Initializes a new instance of the <see cref="MarkdownTextBlock"/> class.
         /// </summary>
+        private long FontSizePropertyToken { get; set; }
+        private long BackgroundPropertyToken { get; set; }
+        private long BorderBrushPropertyToken { get; set; }
+        private long BorderThicknessPropertyToken { get; set; }
+        private long CharacterSpacingPropertyToken { get; set; }
+        private long FontFamilyPropertyToken { get; set; }
+        private long FontStretchPropertyToken { get; set; }
+        private long FontStylePropertyToken { get; set; }
+        private long FontWeightPropertyToken { get; set; }
+        private long ForegroundPropertyToken { get; set; }
+        private long PaddingPropertyToken { get; set; }
+        private long RequestedThemePropertyToken { get; set; }
+        private long SchemeListPropertyToken { get; set; }
+
+
+
         public MarkdownTextBlock()
         {
             // Set our style.
             DefaultStyleKey = typeof(MarkdownTextBlock);
-
-            // Listens for theme changes and updates the rendering.
             themeListener = new Helpers.ThemeListener();
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            // Listens for theme changes and updates the rendering.
             themeListener.ThemeChanged += ThemeListener_ThemeChanged;
 
             // Register for property callbacks that are owned by our parent class.
-            RegisterPropertyChangedCallback(FontSizeProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(BackgroundProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(BorderBrushProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(BorderThicknessProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(CharacterSpacingProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(FontFamilyProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(FontSizeProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(FontStretchProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(FontStyleProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(FontWeightProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(ForegroundProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(PaddingProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(RequestedThemeProperty, OnPropertyChanged);
-            RegisterPropertyChangedCallback(SchemeListProperty, OnPropertyChanged);
+            FontSizePropertyToken = RegisterPropertyChangedCallback(FontSizeProperty, OnPropertyChanged);
+            BackgroundPropertyToken = RegisterPropertyChangedCallback(BackgroundProperty, OnPropertyChanged);
+            BorderBrushPropertyToken = RegisterPropertyChangedCallback(BorderBrushProperty, OnPropertyChanged);
+            BorderThicknessPropertyToken = RegisterPropertyChangedCallback(BorderThicknessProperty, OnPropertyChanged);
+            CharacterSpacingPropertyToken = RegisterPropertyChangedCallback(CharacterSpacingProperty, OnPropertyChanged);
+            FontFamilyPropertyToken = RegisterPropertyChangedCallback(FontFamilyProperty, OnPropertyChanged);
+            FontStretchPropertyToken = RegisterPropertyChangedCallback(FontStretchProperty, OnPropertyChanged);
+            FontStylePropertyToken = RegisterPropertyChangedCallback(FontStyleProperty, OnPropertyChanged);
+            FontWeightPropertyToken = RegisterPropertyChangedCallback(FontWeightProperty, OnPropertyChanged);
+            ForegroundPropertyToken = RegisterPropertyChangedCallback(ForegroundProperty, OnPropertyChanged);
+            PaddingPropertyToken = RegisterPropertyChangedCallback(PaddingProperty, OnPropertyChanged);
+            RequestedThemePropertyToken = RegisterPropertyChangedCallback(RequestedThemeProperty, OnPropertyChanged);
+            SchemeListPropertyToken = RegisterPropertyChangedCallback(SchemeListProperty, OnPropertyChanged);
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            UnhookListeners();
+            themeListener.ThemeChanged -= ThemeListener_ThemeChanged;
+            themeListener = null;
+
+            // Register for property callbacks that are owned by our parent class.
+            UnregisterPropertyChangedCallback(FontSizeProperty, FontSizePropertyToken);
+            UnregisterPropertyChangedCallback(BackgroundProperty, BackgroundPropertyToken);
+            UnregisterPropertyChangedCallback(BorderBrushProperty, BorderBrushPropertyToken);
+            UnregisterPropertyChangedCallback(BorderThicknessProperty, BorderThicknessPropertyToken);
+            UnregisterPropertyChangedCallback(CharacterSpacingProperty, CharacterSpacingPropertyToken);
+            UnregisterPropertyChangedCallback(FontFamilyProperty, FontFamilyPropertyToken);
+            UnregisterPropertyChangedCallback(FontStretchProperty, FontStylePropertyToken);
+            UnregisterPropertyChangedCallback(FontWeightProperty, FontWeightPropertyToken);
+            UnregisterPropertyChangedCallback(ForegroundProperty, ForegroundPropertyToken);
+            UnregisterPropertyChangedCallback(PaddingProperty, PaddingPropertyToken);
+            UnregisterPropertyChangedCallback(RequestedThemeProperty, RequestedThemePropertyToken);
+            UnregisterPropertyChangedCallback(SchemeListProperty, SchemeListPropertyToken);
+            _rootElement = null;
         }
 
         private void ThemeListener_ThemeChanged(Helpers.ThemeListener sender)
